53165f7ab56a2b092739b6abb1f414f9c1fb63c7
Guilherme1777 <guijkg333@gmail.com>
2025-11-25 15:29:30 -0300

    Update LoginActivity.kt

--- app/src/main/java/com/ifpr/androidapptemplate/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ifpr/androidapptemplate/ui/login/LoginActivity.kt
@@ -70,7 +70,7 @@ class LoginActivity : AppCompatActivity() {
 
         // Configuration do Google Sign-In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.
+            .requestIdToen(getString(R.string.
             default_web_client_id))
             .requestEmail()
             .build()
